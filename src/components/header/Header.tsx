7d7e5f08803dc647ea715db5257e17fafd8f4561
Mike Borthwick <mhborthwick@gmail.com>
2023-02-18 14:50:43 -0800

    feat: create nav icon links

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,10 +2,28 @@ import {
   Box,
   Flex,
   Heading,
+  Icon,
+  Link,
   ListItem,
   Spacer,
   UnorderedList,
 } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { AiFillGithub, AiFillLinkedin, AiFillMail } from "react-icons/ai";
+
+function getIconLink(iconType: IconType, url: string) {
+  return (
+    <Link href={url} isExternal>
+      <Icon as={iconType} boxSize={8} />
+    </Link>
+  );
+}
+
+enum URL {
+  GITHUB = "https://github.com/mhborthwick",
+  MAIL = "mailto:mhborthwick *at* gmail -dot- com",
+  LINKED_IN = "https://www.linkedin.com/in/mhborthwick/",
+}
 
 function Header() {
   return (
@@ -21,9 +39,9 @@ function Header() {
       <Spacer />
       <Box as="nav">
         <UnorderedList display={"flex"} listStyleType={"none"} gap={2}>
-          <ListItem>GitHub</ListItem>
-          <ListItem>Mail</ListItem>
-          <ListItem>LinkedIn</ListItem>
+          <ListItem>{getIconLink(AiFillGithub, URL.GITHUB)}</ListItem>
+          <ListItem>{getIconLink(AiFillMail, URL.MAIL)}</ListItem>
+          <ListItem>{getIconLink(AiFillLinkedin, URL.LINKED_IN)}</ListItem>
         </UnorderedList>
       </Box>
     </Flex>
--- src/components/scroll/Scroll.tsx
+++ src/components/scroll/Scroll.tsx
@@ -2,7 +2,7 @@ import { Icon } from "@chakra-ui/react";
 import { AiOutlineArrowDown } from "react-icons/ai";
 
 function Scroll() {
-  return <Icon as={AiOutlineArrowDown} boxSize={6} />;
+  return <Icon as={AiOutlineArrowDown} boxSize={8} />;
 }
 
 export default Scroll;
