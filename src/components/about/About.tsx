b40b652c02954f5e110ee10d85bbae6bf73e29be
Mike Borthwick <mhborthwick@gmail.com>
2023-02-18 11:44:22 -0800

    feat: add scroll icon

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,5 +1,6 @@
 import { Box, Heading, Text, Image, Flex, Spacer } from "@chakra-ui/react";
 import myHeadShot from "../../assets/headshot.png";
+import { Scroll } from "../scroll";
 
 function Circle() {
   const circleStyles = {
@@ -19,29 +20,35 @@ function Circle() {
 
 export function About() {
   return (
-    <Flex alignItems={"center"}>
-      <Box flex={3} textAlign={"left"}>
-        <Heading as="h2">Hello! 👋🏽</Heading>
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
-          velit sapiente eveniet harum ut reiciendis temporibus optio dolorem
-          amet libero ipsum dolor, nam, veritatis ab ea exercitationem quis
-          alias illo.
-        </Text>
-      </Box>
-      <Spacer />
-      <Box position="relative" paddingLeft="2rem" paddingRight="2rem">
-        <Image
-          zIndex={2}
-          boxSize="100px"
-          src={myHeadShot}
-          position="relative"
-          borderRadius="full"
-          alt="Mike Borthwick"
-        />
-        <Circle />
-      </Box>
-    </Flex>
+    <Box>
+      <Flex alignItems={"center"}>
+        <Box flex={3} textAlign={"left"}>
+          <Heading as="h2">Hello! 👋🏽</Heading>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
+            velit sapiente eveniet harum ut reiciendis temporibus optio dolorem
+            amet libero ipsum dolor, nam, veritatis ab ea exercitationem quis
+            alias illo.
+          </Text>
+        </Box>
+        <Spacer />
+        <Box position="relative" paddingLeft="2rem" paddingRight="2rem">
+          <Image
+            zIndex={2}
+            boxSize="100px"
+            src={myHeadShot}
+            position="relative"
+            borderRadius="full"
+            alt="Mike Borthwick"
+          />
+          <Circle />
+        </Box>
+      </Flex>
+      <Flex alignItems={"center"} paddingTop="2rem" paddingBottom="2rem">
+        <Scroll />
+        <Text>Scroll</Text>
+      </Flex>
+    </Box>
   );
 }
 
---
+++ src/components/scroll/Scroll.tsx
@@ -0,0 +1,8 @@
+import { Icon } from "@chakra-ui/react";
+import { AiOutlineArrowDown } from "react-icons/ai";
+
+function Scroll() {
+  return <Icon as={AiOutlineArrowDown} boxSize={6} />;
+}
+
+export default Scroll;
